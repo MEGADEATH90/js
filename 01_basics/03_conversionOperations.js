acfb8805cc3b476921acdbd7587059155a3ab9ec
Kaibalya Preetam Bal <kaibalyapreetambalrj@gmail.com>
2025-06-02 17:26:01 +0000

    coversionOperations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -28,8 +28,24 @@ true => 1; false => 0
 // "" => false
 // "kaibalya" => true
 
-let someNum = 33
-let stringNum = String(someNum)
-console.log(stringNum);
-console.log(typeof stringNum);
+// let someNum = 33
+// let stringNum = String(someNum)
+// console.log(stringNum);
+// console.log(typeof stringNum);
+
+//*********************Operations ********************
+// let value = 3
+// let negValue = -value
+// // console.log(negValue);  // -3
+
+// let str1 = "hello"
+// let str2 = " kaibalya"
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2); // 12
+// console.log(1 + "2"); // 12
+// console.log("1" + 2 + 2); // 122
+// console.log(1 + 2 + "2"); // 32
+
 
