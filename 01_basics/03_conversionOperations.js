7f4cd25e85c4c762fc80ce6207e182e65a4b8e4b
Kaibalya Preetam Bal <kaibalyapreetambalrj@gmail.com>
2025-06-02 16:38:39 +0000

    conversionOperations

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,35 @@
+// let score = 33
+// let score = "33abc"
+// let score = null
+// let score = undefined
+// let score = true
+// console.log(typeof score);
+// console.log(typeof(score));
+
+
+
+// let valueInnumber = Number(score)
+// console.log(typeof valueInnumber);
+// console.log(valueInnumber);
+
+
+
+// Notes
+/*
+"33" => 33
+"33abc" => NaN
+true => 1; false => 0
+*/
+
+// let isLoggedIn = 1
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+// 1 => true; 0 => false
+// "" => false
+// "kaibalya" => true
+
+let someNum = 33
+let stringNum = String(someNum)
+console.log(stringNum);
+console.log(typeof stringNum);
+
