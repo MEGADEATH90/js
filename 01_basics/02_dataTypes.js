7ed664fb148deb1292c24905c54360024485da26
Kaibalya Preetam Bal <kaibalyapreetambalrj@gmail.com>
2025-06-02 13:54:17 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all js code as newer version
+// alert() // we are using nodejs, not brower
+console.log(3 + 3);
+
+// string
+let name = "kaibalya"
+// numeric
+let age = 18
+// boolean
+let isLoggedIn = false
+
+// number => 2 to the power 53
+// bigInt
+// string => ""
+// boolean => true/false
+// null - is considered as a object
+// undefined
+// symbol => unique
+
+// object
+console.log(typeof undefined); // undefinend
+console.log(typeof null); // object
+
