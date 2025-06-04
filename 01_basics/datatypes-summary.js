0d78ba3794822bd68eae2c77a6136745539676cb
Kaibalya Preetam Bal <kaibalyapreetambalrj@gmail.com>
2025-06-04 18:19:23 +0000

    datatypes summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,16 @@
+// Primitve
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+//Reference (Non primitive)
+// Array, Objects, Functions
+const heros = ["shaktiman", "naagraj", "doga"]
+
+let myObj = {
+    name: "kaibalya",
+    age: 22,
+}
+
+const myFun = function(){
+    console.log("Hello world");
+    
+}
