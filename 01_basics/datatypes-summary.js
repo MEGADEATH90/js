abf858460ac46f937248e610243b23f26a5171e7
Kaibalya Preetam Bal <kaibalyapreetambalrj@gmail.com>
2025-06-05 07:06:43 +0000

    update

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -14,3 +14,26 @@ const myFun = function(){
     console.log("Hello world");
     
 }
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//Stack (Primitive), Heap (Non-Primitive)
+
+let myYtName = "Mr__solo_biker"
+let anotherName = myYtName
+console.log(myYtName);
+anotherName = "Mr_biker"
+console.log(anotherName);
+
+
+
+let user1 = {
+    email: "user@gmail.com",
+    upi: "user@ybl"
+}
+let user2 = user1
+
+user2.email = "kai@gmail.com"
+console.log(user1.email);
+console.log(user2.email);
+
